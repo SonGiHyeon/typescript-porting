8cf7be59b4548a5bf43e650d3511263fd13ab2de
songihyeon <tyumr70@gmail.com>
2025-03-07 19:15:01 +0900

    task compelted

--- src/part1.ts
+++ src/part1.ts
@@ -1,26 +1,28 @@
 // Todo: FILL_ME_IN을 타입에 맞도록 값을 넣어 수정하세요.
 const FILL_ME_IN = null;
 
-export let isDone: boolean = FILL_ME_IN;
-export let isShow: boolean = FILL_ME_IN;
+export let isDone: boolean = false;
+export let isShow: boolean = true;
 
-export let num1: number = FILL_ME_IN;
-export let num2: number = FILL_ME_IN;
+export let num1: number = 7;
+export let num2: number = 0.6878;
 
-export let str: string = FILL_ME_IN;
+export let str: string = 'hello';
 
-export let list1: number[] = [FILL_ME_IN];
-export let list2: Array<number> = [FILL_ME_IN];
+export let list1: number[] = [1, 2, 3];
+export let list2: Array<number> = [1, 2, 3];
 
-export let list3: string[] = [FILL_ME_IN];
-export let list4: Array<string> = [FILL_ME_IN];
+export let list3: string[] = ['banana', 'apple', 'mango'];
+export let list4: Array<string> = ['banana', 'apple', 'mango'];
 
 export let list5: [number, string, boolean] = [
-  FILL_ME_IN,
-  FILL_ME_IN,
-  FILL_ME_IN,
+  1, 'banana', true
 ];
 
-export let obj: object = FILL_ME_IN;
+export let obj: object = {};
 export let obj2: { name: string; age: number; isDeveloper: boolean } =
-  FILL_ME_IN;
+{
+  name: 'alice',
+  age: 20,
+  isDeveloper: true,
+};
--- src/part2.ts
+++ src/part2.ts
@@ -1,13 +1,11 @@
-// Todo: FILLMEIN을 타입에 맞도록 수정하여 오류 메세지가 뜨지 않도록 합니다.
-
-export function sumNumber(a: FILLMEIN, b: FILLMEIN): FILLMEIN {
+export function sumNumber(a: number, b: number): number {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('Invalid type detected. Only numbers are allowed.');
   }
   return a + b;
 }
 
-export let sumString = (first: FILLMEIN, last: FILLMEIN): FILLMEIN => {
+export let sumString = (first: string, last: string): string => {
   if (typeof first !== 'string' || typeof last !== 'string') {
     throw new Error('Invalid type detected. Only strings are allowed.');
   }
@@ -15,7 +13,7 @@ export let sumString = (first: FILLMEIN, last: FILLMEIN): FILLMEIN => {
   return `${first} ${last}`;
 };
 
-export function isBoolean(a: FILLMEIN, b: FILLMEIN): FILLMEIN {
+export function isBoolean(a: boolean, b: boolean): boolean {
   if (typeof a !== 'boolean' || typeof b !== 'boolean') {
     return true;
   } else {
@@ -23,10 +21,10 @@ export function isBoolean(a: FILLMEIN, b: FILLMEIN): FILLMEIN {
   }
 }
 
-export function concatArray(arr1: FILLMEIN, arr2: FILLMEIN): FILLMEIN {
+export function concatArray<T>(arr1: T[], arr2: T[]): T[] {
   return [...arr1, ...arr2];
 }
 
-export function mergeObjects(obj1: FILLMEIN, obj2: FILLMEIN): FILLMEIN {
+export function mergeObjects<T extends object, U extends object>(obj1: T, obj2: U): T & U {
   return { ...obj1, ...obj2 };
 }
